c2845b828ab4582ba25baf673d185ed4fbce0a74
Victor Haynes <haynesvic@gmail.com>
2022-09-27 15:23:59 -0500

    updated error message of OrderDetail model when trying to order the same product more than once (validation message)

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,5 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :product_id, uniqueness: true
+  validates :product_id, uniqueness: { message: "out of stock. Already purchased."}
 end
